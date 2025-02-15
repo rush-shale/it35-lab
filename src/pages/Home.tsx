664594fa55a160b60fe5e4aaa3f6be5a90e5a6ca
rush-shale <Hinoyogjohnrushel@gmail.com>
2025-02-15 15:23:57 +0800

    added home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+
+import { 
+  IonButtons,
+  IonButton,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+return (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonButtons slot='start'>
+          <IonMenuButton></IonMenuButton>
+        </IonButtons>
+        <IonTitle>Home</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent fullscreen>
+  <IonButton routerLink="/it35-lab/app/home/details" expand="full">
+          Details
+</IonButton>
+    </IonContent>
+  </IonPage>
+);
 };
 
 export default Home;
