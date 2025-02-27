b16112933d6dd66e2e91a836f05c96266876fdef
rush-shale <Hinoyogjohnrushel@gmail.com>
2025-02-27 16:50:11 +0800

    added changes and routing changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,37 @@
-import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonRouter
+} from '@ionic/react';
+function Login() {
+  const navigation = useIonRouter();
+  const doLogin = () => {
+    navigation.push('/it35-lab/app', 'forward', 'replace');
+  };
+
     return (
       <IonPage>
         <IonHeader>
           <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
             <IonTitle>Login</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
+          <IonButton onClick={() => doLogin()} expand="full">
+            Login
+          </IonButton>
         </IonContent>
       </IonPage>
     );
-  };
+  }
   
-  export default Login; 
+  export default Login;
