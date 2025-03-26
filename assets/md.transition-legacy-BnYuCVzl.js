09ca7ed3d676a55b50cf5a97e3f30fdea24dce78
rush-shale <Hinoyogjohnrushel@gmail.com>
2025-03-26 19:12:30 +0800

    Updates

--- assets/md.transition-legacy-BnYuCVzl.js
+++ assets/md.transition-legacy-BnYuCVzl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CXvY98Gd.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-Bb7gRxXe.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
