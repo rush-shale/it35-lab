178d75c33464ba5b4ec1e44f0986ef2e214ac4eb
rush-shale <Hinoyogjohnrushel@gmail.com>
2025-04-29 10:18:40 +0800

    added feedcontainer

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -7,6 +7,7 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+  import FeedContainer from '../..components/FeedContainer';
   const Feed: React.FC = () => {
     return (
       <IonPage>
