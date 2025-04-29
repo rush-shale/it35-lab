2e23504274365295e7b4786e239689202e7ae40d
rush-shale <Hinoyogjohnrushel@gmail.com>
2025-04-29 10:35:36 +0800

    added feedcontainer

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,26 +1,30 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  import FeedContainer from '../..components/FeedContainer';
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <div
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+// Make sure this path is correct
+import FeedContainer from '../../components/FeedContainer';
+
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        {/* Remove this div if you don't need static "Feed" text */}
+        {/* <div
           style={{
             display: 'flex',
             alignItems: 'center',
@@ -29,9 +33,13 @@ import {
           }}
         >
           Feed
-        </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  export default Feed;
+        </div> */}
+        
+        {/* Proper usage of FeedContainer */}
+        <FeedContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
